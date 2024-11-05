9ebce108b8fe5267a9912fffa95c8ea4397ac52a
lzkgmr <eikhromova@edu.hse.ru>
2024-11-05 14:28:07 +0300

    HW-3: added a cell in the table

--- eikhromovaPW2/eikhromovaPW2/WishStoringViewController.swift
+++ eikhromovaPW2/eikhromovaPW2/WishStoringViewController.swift
@@ -12,13 +12,14 @@ final class WishStoringViewController: UIViewController {
     // MARK: Enum(Constants)
     private enum Constants {
         static let tableCornerRadius: CGFloat = 20
-        static let tableOffsetVertical: CGFloat = 110
-        static let tableOffsetHorizontal: CGFloat = 40
+        static let tableOffsetV: CGFloat = 110
+        static let tableOffsetH: CGFloat = 40
     }
     
     // MARK: Fields
     var backgroundColor: UIColor?
     private let table: UITableView = UITableView(frame: .zero)
+    private var wishArray: [String] = ["I wish to add cells to the table"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,25 +29,32 @@ final class WishStoringViewController: UIViewController {
     
     private func configureTable() {
         view.addSubview(table)
-        table.backgroundColor = .red
+        table.backgroundColor = .darkGray
         table.dataSource = self
         table.separatorStyle = .none
         table.layer.cornerRadius = Constants.tableCornerRadius
         
+        table.pinVertical(to: view, Constants.tableOffsetV)
+        table.pinHorizontal(to: view, Constants.tableOffsetH)
         
-        table.pinVertical(to: view, Constants.tableOffsetVertical)
-        table.pinHorizontal(to: view, Constants.tableOffsetHorizontal)
+        table.register(WrittenWishCell.self,forCellReuseIdentifier: WrittenWishCell.reuseId)
     }
 }
 
 // MARK: - UITableViewDataSource
 extension WishStoringViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return wishArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: WrittenWishCell.reuseId, for: indexPath)
+        
+        guard let wishCell = cell as? WrittenWishCell else { return cell }
+        
+        wishCell.configure(with: wishArray[indexPath.row])
+        
+        return wishCell
     }
     
 }
